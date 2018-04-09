43b65a19a299ac3f47c27bacf84e2b820703db66
tanner0101 <me@tanner.xyz>
2018-04-09 19:51:11 -0400

    remove deprecations

--- Sources/TemplateKit/Deprecated.swift
+++ Sources/TemplateKit/Deprecated.swift
@@ -1,18 +1,1 @@
-
-/// Deprecated.
-@available(*, deprecated, renamed: "TemplateKitError")
-public typealias TemplateError = TemplateKitError
-
-extension TemplateKitError {
-    /// Deprecated.
-    @available(*, deprecated, renamed: "init(identifier:reason:source:)")
-    public static func parse(reason: String, template: TemplateSource, source: SourceLocation) -> TemplateKitError {
-        return TemplateKitError(identifier: "parse", reason: reason, source: template)
-    }
-
-    /// Deprecated.
-    @available(*, deprecated, renamed: "init(identifier:reason:source:)")
-    public static func serialize(reason: String, template: TemplateSource, source: SourceLocation) -> TemplateKitError {
-        return TemplateKitError(identifier: "serialize", reason: reason, source: template)
-    }
-}
+// Nothing here yet...
