70d73d2ddf7fd22e18947895601fa8f822e7afb2
bre7 <mobileuy@gmail.com>
2018-04-20 19:14:11 -0300

    Added ViewRenderer

---
+++ Sources/TemplateKit/Utilities/ViewRenderer.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+///
+/// View renderers are responsible for loading
+/// the files paths given and caching if needed.
+///
+/// View renderers are also responsible for
+/// accepting a Node for templated responses.
+public protocol ViewRenderer: class {
+
+    /// For view renderers that use a cache to
+    /// optimize view loads, use this variable
+    /// to toggle whether or not cache should be
+    /// implemented
+    ///
+    /// Normally, cache is disabled in development
+    /// so views can be tested w/o recompilation.
+    /// In production, cache is enabled to optimize
+    /// view serving speed.
+    var shouldCache: Bool { get set }
+
+    /// Renders the template bytes into a view using the supplied `Encodable` object as context.
+    ///
+    /// - parameters:
+    ///     - path: Path to the template.
+    ///     - context: `Encodable` item that will be encoded to `TemplateData` and used as template context.
+    /// - returns: `Future` containing the rendered `View`.
+    func render<E>(_ path: String, _ context: E) -> Future<View> where E: Encodable
+}
