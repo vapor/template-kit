c411072b9c880b6f902229160549462e01b51652
tanner0101 <me@tanner.xyz>
2018-08-08 12:35:45 -0400

    add userInfo to TemplateDataContext

--- Sources/TemplateKit/AST/TemplateDataContext.swift
+++ Sources/TemplateKit/AST/TemplateDataContext.swift
@@ -2,9 +2,13 @@
 public final class TemplateDataContext {
     /// The referenced `TemplateData`
     public var data: TemplateData
+    
+    /// User-defined storage.
+    public var userInfo: [AnyHashable: Any]
 
     /// Create a new `TemplateDataContext`.
-    public init(data: TemplateData) {
+    public init(data: TemplateData, userInfo: [AnyHashable: Any] = [:]) {
         self.data = data
+        self.userInfo = userInfo
     }
 }
